4385f52efd27309ff332844b162c4c40f9cc7fef
Hampus Wall <31284797+carlhampuswall@users.noreply.github.com>
2024-03-22 17:32:47 +0100

    Moved state track functionality from __init__ to coordinator to be able to update after addition or updates to or of apps. (#26)

--- custom_components/smartknob/coordinator.py
+++ custom_components/smartknob/coordinator.py
@@ -1,5 +1,6 @@
 """Coordinator."""
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, State
+from homeassistant.helpers.event import async_track_state_change
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from .const import DOMAIN
@@ -24,6 +25,7 @@ class SmartknobCoordinator(DataUpdateCoordinator):
 
     async def async_update_app_config(self, data: dict = None):
         """Update config for app."""
+
         if self.store.async_get_app(data.get("app_id")):
             self.store.async_update_app(data)
             return
@@ -37,3 +39,36 @@ class SmartknobCoordinator(DataUpdateCoordinator):
     async def async_delete_config(self):
         """Delete config."""
         await self.store.async_delete()
+
+    async def update(self):
+        """Update state tracker."""
+        # Subsribe to entity state changes of all entities used in knobs
+
+        mqtt = self.hass.data[DOMAIN]["mqtt_handler"]
+
+        knobs = self.store.async_get_knobs()
+        entity_ids = [
+            (app["entity_id"]) for knob in knobs.values() for app in knob["apps"]
+        ]
+
+        async def async_state_change_callback(
+            entity_id, old_state: State, new_state: State
+        ):
+            """Handle entity state changes."""
+            affected_knobs = []
+            apps = []
+
+            if new_state.context.user_id is None:
+                return
+
+            for knob in knobs.values():
+                for app in knob["apps"]:
+                    if app["entity_id"] == entity_id:
+                        affected_knobs.append(knob)
+                        apps.append(app)  # THIS DOESNT REALLY WORK WILL WORK FOR NOW
+
+            await mqtt.async_entity_state_changed(
+                affected_knobs, apps, old_state, new_state
+            )
+
+        async_track_state_change(self.hass, entity_ids, async_state_change_callback)
